a464e42b102cdafaa54cab736e8f0fc6a411370e
Ela Chabko <elachabko@gmail.com>
2022-12-25 18:45:01 +0100

    initial commit

---
+++ register.js
@@ -0,0 +1,36 @@
+
+ 
+        var mail = document.getElementById("user-email");
+        var mailConf = document.getElementById("user-email-confirmation");
+        var nick = document.getElementById("nickname");
+        var pass = document.getElementById("password");
+
+
+        if (mailConf != mail)
+        {
+          alert("Błędny adres e-mail")
+        }
+
+
+
+
+        {/* {  
+  for(var i = 0; i < users.length; i++){
+    if((user == users[i]) && (pass == passwords[i]))
+      return true;
+  }
+  return false;
+}
+function check()
+  var user = document.form1.user.value;
+  var pass = document.form1.pass.value;
+  if(!sprawdz_haslo(user, pass)){
+      alert('Niepoprawne hasło!');
+  } 
+  else {
+    document.location.href = "log-on.html";
+  } */}
+        {/* document.cookie = "nazwa=wartosc"
+        document.cookie = encodeURIComponent("nazwa użytkownika") + "=" + encodeURIComponent("Karol Nowak");
+        console.log(document.cookie); //"nazwa%20u%C5%BCytkownika=Karol%20Nowak"
+         */}
